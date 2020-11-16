b8aedea40e16770937235a41573feba612265a26
iasl <gmlejr456@naver.com>
2020-11-16 15:19:43 +0900

    scripts update

--- scripts/processing_data.py
+++ scripts/processing_data.py
@@ -2,6 +2,11 @@ import sys
 import pyzed.sl as sl
 import numpy as np
 import cv2
+import os
+import re
+
+width = 1920
+height = 1080
 
 def progress_bar(percent_done, bar_length=50):
     done_length = int(bar_length * percent_done / 100)
@@ -9,92 +14,84 @@ def progress_bar(percent_done, bar_length=50):
     sys.stdout.write('[%s] %.2f%s' % (bar, percent_done, '%'))
     sys.stdout.flush()
 
+def extract_center_coordi_basic(center_index, depth_map):
+    frame_idx, x1, y1, x2, y2 = center_index
 
+    box = depth_map[x1 : x2, y1 : y2]
+    max_depth = np.max(box)
+    max_index = np.unravel_index(box.argmax(), box.shape)
+    min_depth = np.min(box)
+    min_index = np.unravel_index(box.argmin(), box.shape)
+
+    center_x = (x1 + x2) // 2 
+    center_y = (y1 + y2) // 2
+    center_z = (max_depth + min_depth) / 2
+    
+    return center_x, center_y, center_z 
+
+def transform_box2D_to_index(target_coordi):
+    
+    # load target_coordinates 
+    target_coordi = open(target_coordi, 'r')
+    lines = target_coordi.readlines()
+    coordi_index = []
+    count = 0 
+    for line in lines:
+        
+        # target 이 비어있는 경우 고려해주어야 함
+        if count != int(line.split()[0]):
+            coordi_index.extend([count,0,0,height-1, width-1])
+            count += 1
+            continue
+
+        x1 = line.split()[1]
+        if x1[0] == '[':
+            x1 = x1[1:]
+        y1 = line.split()[2]
+        x2 = line.split()[3]
+        y2 = line.split()[4]
+        if y2[-1] ==']':
+            y2 = y2[:len(y2)-1]
+
+        y1_ind = int(float(x1)*width)
+        y2_ind = int(float(x2)*width)
+        x1_ind = int(float(y1)*height)
+        x2_ind = int(float(y2)*height)
+
+        if x2_ind == height:
+            x2_ind -= 1
+        if y2_ind == height:
+            y2_ind -= 1
+
+        coordi_index.extend([count, x1_ind, y1_ind, x2_ind, y2_ind])
+        count += 1
+    
+    return np.array(coordi_index).reshape(len(coordi_index)//5,5) 
 
 def main():
-    depth_uint16 = "/home/jwk6844/objecet_detection/3dod_zedm/pngfile/depth000001.png"
-    depth_float32 = "/home/jwk6844/objecet_detection/3dod_zedm/depthfile/depth_data000001.txt"
-    target_coordi = "/home/jwk6844/objecet_detection/3dod_zedm/avifile/test_target_coordi.txt"
-    left_img = "/home/jwk6844/objecet_detection/3dod_zedm/pngfile/left000001.png"   
+   
+    depth_float32 = "/home/iasl/object_detection/yolo_tf2/3dod_zedm/depthfile/depth_data000001.txt"
+    target_coordi = "/home/iasl/object_detection/yolo_tf2/3dod_zedm/avifile/test_target_coordi.txt"
+    left_img = "/home/iasl/object_detection/yolo_tf2/3dod_zedm/pngfile/left000001.png"   
 
-    depth_uint16 = cv2.imread(depth_uint16, cv2.IMREAD_ANYDEPTH)
     depth_float32 = np.loadtxt(depth_float32, dtype=np.float32)
     left_img = cv2.imread(left_img, cv2.IMREAD_UNCHANGED)
 
-    print('shape depth_uint16 : {}'.format(depth_uint16.shape))
-    print('type depth_uint16 : {}'.format(depth_uint16.dtype))
-    print('depth_uint16[100][100] : {}\n'.format(depth_uint16[100][100]))
-
-    print('shape depth_float32 : {}'.format(depth_float32.shape))
-    print('type depth_float32 : {}'.format(depth_float32.dtype))
-    print('depth_float32[100][100] : {}\n'.format(depth_float32[100][100]))
-
-    print('shape left_img : {}'.format(left_img.shape))
-    print('type left_img : {}'.format(left_img.dtype))
-    print('left_img[100][100] : {}\n'.format(left_img[100][100]))
-
-
-    files = open(target_coordi, 'r')
-    lines = files.readlines()
-    count =0 
-    for line in lines:
-        # print('line{} : {}'.format(count, line.strip()))
-        # print(line.split())
-        if count == 0:
-            x1 = line.split()[1]
-            if x1[0] == '[':
-                x1 = x1[1:]
-            y1 = line.split()[2]
-            x2 = line.split()[3]
-            y2 = line.split()[4]
-            if y2[-1] ==']':
-                y2 = y2[:len(y2)-1]
-        count +=1
+    # extract box index
+    center_index = transform_box2D_to_index(target_coordi)
     
-    y1_ind = int(float(x1)*1920)
-    y2_ind = int(float(x2)*1920)
-    x1_ind = int(float(y1)*1080)
-    x2_ind = int(float(y2)*1080)-1
-    # print('x1 : {} -> y1 index : {} '.format(x1,x1_ind))
-    # print('x2 : {} -> y2 index : {} '.format(x2,x2_ind))
-    # print('y1 : {} -> x1 index : {} '.format(y1,y1_ind))
-    # print('y2 : {} -> x2 index : {}\n '.format(y2,y2_ind))  
+    frame_idx, x1, y1, x2, y2 = center_index[0]
+
+    # extract center coordinate
+    center_x, center_y, center_z = extract_center_coordi_basic(center_index[0], depth_float32)
+
+
+
 
     cut_img = np.zeros((1080, 1920,4)).astype(np.uint8)
-    cut_img[x1_ind:x2_ind, y1_ind:y2_ind, :] = left_img[x1_ind:x2_ind, y1_ind:y2_ind, :]
+    cut_img[x1 : x2, y1 : y2, :] = left_img[x1 : x2, y1 : y2, :]
 
-
-
-    box_uint16 = depth_uint16[x1_ind:x2_ind,y1_ind:y2_ind]
-    box_float32 = depth_float32[x1_ind:x2_ind,y1_ind:y2_ind]
-
-    max_depth_uint16 = np.max(box_uint16)
-    max_depth_float32 = np.max(box_float32)
-    max_index_uint16 = np.unravel_index(box_uint16.argmax(), box_uint16.shape)
-    max_index_float32 = np.unravel_index(box_float32.argmax(), box_float32.shape)
-    
-    min_depth_uint16 = np.min(box_uint16)
-    min_depth_float32 = np.min(box_float32)
-    min_index_uint16 = np.unravel_index(box_uint16.argmin(), box_uint16.shape)
-    min_index_float32 = np.unravel_index(box_float32.argmin(), box_float32.shape)
-
-    print('max uint16 : {}, min uint16 : {} '.format(max_depth_uint16, min_depth_uint16))
-    print('max index : {}, min index : {} '.format(max_index_uint16, min_index_uint16))
-    print('max float32 : {}, min float32 : {} '.format(max_depth_float32, min_depth_float32))
-    print('max index : {}, min index : {} \n'.format(max_index_float32, min_index_float32))
-
-    center_uint16_x = (x1_ind + x2_ind) // 2 
-    center_uint16_y = (y1_ind + y2_ind) // 2
-    center_uint16_z = (max_depth_uint16 + min_depth_uint16) / 2 
-
-    center_float32_x = (x1_ind + x2_ind) // 2 
-    center_float32_y = (y1_ind + y2_ind) // 2
-    center_float32_z = (max_depth_float32 + min_depth_float32) / 2
-
-    print('Center uint16 : ({},{}), depth : {}mm'.format(center_uint16_x, center_uint16_y, center_uint16_z))
-    print('Center float32 : ({},{}), depth : {}mm'.format(center_float32_x, center_float32_y, center_float32_z))
-
-    cut_img[center_float32_x, center_float32_y,:] = [255, 255, 255, 0]
+    cut_img[center_x, center_y,:] = [255, 255, 255, 0]
 
 
 
